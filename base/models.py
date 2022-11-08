ec3ace39b28bda317b591e0054a33fec3ce032b6
Brian Mungai <bryanmungai254@gmail.com>
2022-11-08 13:55:37 +0300

    created base app, configured it and created a usermodel from django-abstractuser

---
+++ base/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create a new user models from the inbuilt django usermodel
+
+
+class User(AbstractUser):
+    pass
